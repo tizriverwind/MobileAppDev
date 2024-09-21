ffec01f3c0b51811e6d75a71b03f1b0813376078
tizriverwind <thisisriverwind@gmail.com>
2024-09-21 14:24:19 -0700

    HelloWorld A1

---
+++ app/README.md
@@ -0,0 +1,23 @@
+
+# My Android App
+
+## Description
+My Android App is a mobile application developed as part of the curriculum for CS5520: Mobile App Development at Northeastern University. 
+This app is crafted using Android Studio and Java, with continuous updates and new features being added throughout the Fall 2024 semester. Currently, the app includes a personalized logo, displays a "Hello World" text, and features an "About Me" Toast function, which provides a brief introduction to the developer.
+
+## Features
+
+- Additional features as applicable.
+
+## Installation
+To install this application, follow these steps:
+
+1. **Clone the Repository**:
+   ```bash
+   git clone https://github.com/tizriverwind/MobileAppDev.git
+   ```
+2. **Open and Run Project in Android Studio**:
+    - Open Android Studio.
+    - Choose `Open an Existing Project` and navigate to the folder where you cloned the project.
+    - Build the project by going to `Build > Make Project`.
+    - Run the app on a physical device or an emulator by clicking on the `Run > Run 'app'` button.
---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "Xiaolin Liu\n" + "liu.xiaolin@northeastern.edu",
+
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+    
+}
